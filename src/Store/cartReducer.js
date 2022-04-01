0280abc7496efc64241a0f20072412073c75aaca
parth maheta <35091091+parthmaheta@users.noreply.github.com>
2022-04-01 12:07:42 +0530

    added cart reducer

---
+++ src/Store/cartReducer.js
@@ -0,0 +1,23 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from "./constants"
+
+const initialState = []
+
+export function cartReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TO_CART:
+      const index = state.findIndex(
+        (product) => product.id == action.payload.id
+      )
+      if (index > -1) {
+        state[index].quantity += action.payload.quantity
+      } else {
+        state.push(action.payload)
+      }
+      return [...state]
+    case REMOVE_FROM_CART:
+      return [...state.filter((product) => product.id !== action.payload.id)]
+
+    default:
+      return state
+  }
+}
